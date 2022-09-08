9721dfcf9082bfbabf188b48a137eaa1efc41778
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-09-08 22:48:51 +0200

    Update function file

---
+++ 3.function.sh
@@ -0,0 +1,17 @@
+# FUNCTION
+
+# 1) function without parameters
+
+function sayHello() {
+    echo "Hello World!"
+}
+
+sayHello
+
+# 2) function with parameters
+
+function greeting() {
+    echo "Hello, I am $1 and I am $2 years old"
+}
+
+greeting "Joao" "100"
